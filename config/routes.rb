6e29ff1c11c83be7008c4dc74ca3ab0da804bd74
David Wu <david204code@gmail.com>
2019-08-28 21:44:42 +0100

    edited the config routes.rb page, change the http page name

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#home'
 
-  get 'pages/home'
-  get 'pages/about'
+  get '/home', to: 'pages#home'
+  get '/about', to: 'pages#about'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
