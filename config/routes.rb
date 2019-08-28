af027128b217ca8f2677b213f080e6e66a24cdd4
David Wu <david204code@gmail.com>
2019-08-28 22:13:27 +0100

    Created the activites page, route and controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   get '/home', to: 'pages#home'
   get '/about', to: 'pages#about'
+  get '/activities', to: 'pages#activities' 
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
