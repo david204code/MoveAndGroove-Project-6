9b06a52a8ad7565f824cb5cbb1e520b838455572
David Wu <david204code@gmail.com>
2019-08-31 00:47:27 +0100

    Added validation of the Activity model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,2 +1,6 @@
 class Activity < ApplicationRecord
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :intensity, presence: true 
+  validates :intensity, presence: true 
 end
