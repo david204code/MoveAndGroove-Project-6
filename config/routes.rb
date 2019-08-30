9a5c2848cbab67657c001684329882d2378b674a
David Wu <david204code@gmail.com>
2019-08-30 18:01:24 +0100

    Generating the Activity controller (singular) via scaffold, rerouted the pages activites controller

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,9 @@ Rails.application.routes.draw do
 
   get '/home', to: 'pages#home'
   get '/about', to: 'pages#about'
-  get '/activities', to: 'pages#activities' 
+  get '/listofactivities', to: 'pages#activities' 
 
-
+  resources :activities
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
